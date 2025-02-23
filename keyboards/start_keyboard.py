6489e0f75960b4af16ad4963326af90687aae16e
Alexey <bazooka2008@inbox.ru>
2025-02-23 14:48:39 +0300

    и еще одно наиважнейшее изменение

--- keyboards/start_keyboard.py
+++ keyboards/start_keyboard.py
@@ -1,5 +1,6 @@
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from config import DONATE_LINK
 
 
 def start_keyboard():
@@ -8,5 +9,5 @@ def start_keyboard():
     builder.row(InlineKeyboardButton(text="Подробнее о приюте", callback_data="start_about"),
                 InlineKeyboardButton(text="Связь с нами", callback_data="start_contact"))
     builder.row(InlineKeyboardButton(text="Поддержать нас",
-                                     url=f"https://yandex.ru/"))
+                                     url=DONATE_LINK))
     return builder.as_markup()
