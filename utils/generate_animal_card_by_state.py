42edad3e392288c13c0e86c84352395e4e20047f
Kog3000 <arinaazeva9@gmail.com>
2025-03-18 10:12:25 +0300

    настройка год\лет\месяцев

--- utils/generate_animal_card_by_state.py
+++ utils/generate_animal_card_by_state.py
@@ -16,7 +16,24 @@ async def generate_animal_card_by_state(data, message: Message):
     now = date.today()
     delta = now - birthday
 
-    text = f"""Привет! Я {name}, {get_text_gender(gender)}, мне {delta.days // 365} лет.\n{description}\nТеги: {", ".join(map(lambda x: x.tag, tags))}"""
+    age = delta.days // 365
+    month = delta // 30 - age * 12
+
+    age_state = 'лет'
+    month_state = 'месяцев' if month not in (0, 1) else 'месяц'
+    if age < 1:
+        age, age_state = month, month_state
+    elif age == 1:
+        age_state = 'год'
+    elif 1 < age < 5:
+        age_state = 'года'
+    elif age >= 5:
+        age_state = 'лет'
+
+    if month:
+        text = f"""Привет! Я {name}, {get_text_gender(gender)}, мне {age} {age_state} {month} {month_state}.\n{description}\nТеги: {", ".join(map(lambda x: x.tag, tags))}"""
+    else:
+        text = f"""Привет! Я {name}, {get_text_gender(gender)}, мне {age} {age_state}.\n{description}\nТеги: {", ".join(map(lambda x: x.tag, tags))}"""
 
     album_builder = MediaGroupBuilder(
         caption=text
