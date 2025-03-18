d7ad0381ae15cfba118d59dd3bd572d2de03a5ab
Gleb <gabramov723@gmail.com>
2025-03-18 11:09:10 +0300

    fix

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,7 @@
 from aiogram import Dispatcher
 from .commands_handlers import router as commands_router
 from .default_callback_handlers import router as default_callback_router
-from .admin import animals_callback_router, add_animal_router, delete_animal_router, 
-                update_animal_router, guardianship_router, users_control_router,
-                users_control_callback_router, publish_news_callback_router,
-                publish_news_router, stats_callback, publish_volunteer_news_callback_router, publish_volunteer_news_router
+from .admin import animals_callback_router, add_animal_router, delete_animal_router, update_animal_router, guardianship_router, users_control_router, users_control_callback_router, publish_news_callback_router, publish_news_router, stats_callback, publish_volunteer_news_callback_router, publish_volunteer_news_router
 from .user import users_callback_router, watch_animals_handler
 
 
