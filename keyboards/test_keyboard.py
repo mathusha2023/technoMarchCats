a55d33ea7ae875d77fb5af0a1a615377ed1d1d9b
Gleb <gabramov723@gmail.com>
2025-03-18 23:00:32 +0300

    Наверное финальный тест

--- keyboards/test_keyboard.py
+++ keyboards/test_keyboard.py
@@ -1,24 +1,10 @@
-from aiogram.types import InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
-def test_keyboard():
-    builder = InlineKeyboardBuilder()
-    builder.row(InlineKeyboardButton(text="А", callback_data="А"))
-    builder.row(InlineKeyboardButton(text="Б", callback_data="Б"))
-    builder.row(InlineKeyboardButton(text="В", callback_data="В"))
-    return builder.as_markup()
-
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def test_reply_keyboard():
     buttons = [
-        [KeyboardButton(text="А")],
-        [KeyboardButton(text="Б")],
-        [KeyboardButton(text="В")],
-        [KeyboardButton(text="🚫 Отмена")]
+        [KeyboardButton(text="А"), KeyboardButton(text="Б"), KeyboardButton(text="В"), KeyboardButton(text="🚫 Отмена")]
     ]
     builder = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
     return builder
+
 
