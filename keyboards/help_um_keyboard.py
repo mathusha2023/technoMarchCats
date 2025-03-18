b932c3e16860bda35a68a83780a0f1bed5d90505
Alex Efremenkov <top.chel.05@mail.ru>
2025-03-18 20:08:43 +0300

    ssss

--- keyboards/help_um_keyboard.py
+++ keyboards/help_um_keyboard.py
@@ -1,5 +1,7 @@
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
 # from config import DONATE_LINK
 
 
@@ -8,8 +10,8 @@ def help_um_keyboard(is_volunteer):
 
     builder = InlineKeyboardBuilder()
     # builder.row(InlineKeyboardButton(text="Помочь копейкой", url=DONATE_LINK))
-    builder.row(InlineKeyboardButton(text="Поддержать нас копейкой", callback_data="fast_pay"))
-    builder.row(InlineKeyboardButton(text="Помочь вещами", callback_data="partners_links_list"))
+    builder.row(InlineKeyboardButton(text="Поддержать нас копейкой", callback_data="fast_pay"),
+                InlineKeyboardButton(text="Помочь вещами", callback_data="partners_links_list"))
     builder.row(InlineKeyboardButton(text=volunteer_text, callback_data="volunteer"))
     builder.row(InlineKeyboardButton(text="◀️ Назад",
                                      callback_data="start"))
