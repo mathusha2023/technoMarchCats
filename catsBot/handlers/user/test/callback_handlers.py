9a2e72431b5dd52dae425032fbb5f9ba57d8852a
Gleb <gabramov723@gmail.com>
2025-03-19 10:58:44 +0300

    рпач

--- catsBot/handlers/user/test/callback_handlers.py
+++ catsBot/handlers/user/test/callback_handlers.py
@@ -8,6 +8,9 @@ from utils.generate_animal_card_by_state import generate_animal_card_by_state
 from utils.main_info import get_animal_info
 import strings
 from filters import StatesGroupFilter
+from data import db_session
+from data.users import User
+from data.animals import Animal
 
 router = Router()
 
@@ -101,7 +104,6 @@ async def answer(message: Message, state: FSMContext):
     
     await generate_animal_card_by_state(main_info, message)
     await message.answer("можете оставить заявку", reply_markup=keyboards.final_test_keyboard())
-    await message.answer()
     
 @router.callback_query(F.data, StatesGroupFilter(TestStates))
 async def take(callback_query: CallbackQuery, state: FSMContext):
@@ -110,7 +112,7 @@ async def take(callback_query: CallbackQuery, state: FSMContext):
     await callback_query.message.answer(strings.GREETING, reply_markup=keyboards.start_keyboard())
     await state.clear()
     await state.set_state(TestStates.test)
-    await message.answer()
+
     
 @router.message(F.text, StatesGroupFilter(TestStates))
 async def badInput(message: Message, state: FSMContext):
--- catsBot/keyboards/final_test_keyboard.py
+++ catsBot/keyboards/final_test_keyboard.py
@@ -1,9 +1,11 @@
-from aiogram.types import InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def final_test_keyboard():
-    builder = InlineKeyboardBuilder()
-    builder.row(InlineKeyboardButton(text="взять", callback_data='take'), InlineKeyboardButton(text="в меню", callback_data='menu'))
-    return builder.as_markup()
-
+    buttons = [
+        [KeyboardButton(text="взять"),
+        KeyboardButton(text="в меню")]
+    ]
+    builder = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+    return builder
