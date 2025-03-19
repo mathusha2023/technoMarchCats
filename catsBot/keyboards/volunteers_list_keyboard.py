c56451230cd56de5dafc02892c9966439e8c99ae
Kog3000 <arinaazeva9@gmail.com>
2025-03-19 11:59:00 +0300

    дфые

--- catsBot/keyboards/volunteers_list_keyboard.py
+++ catsBot/keyboards/volunteers_list_keyboard.py
@@ -14,7 +14,7 @@ def volunteers_list_keyboard(users, fourth_number, show_left=False, show_right=F
             row.clear()
 
 
-    row = [InlineKeyboardButton(text="◀️ Назад", callback_data="volunteers_interaction")]
+    row = [InlineKeyboardButton(text="Назад", callback_data="volunteers_interaction")]
     if show_left:
         row.insert(0, InlineKeyboardButton(text="◀️",
                                            callback_data=f"volunteer_control_left_{fourth_number}"))  # номер четверки тегов
