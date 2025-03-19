fcf5ee6f0c4017cd0c0eb22b8ea7c766dc5eb92a
Kog3000 <arinaazeva9@gmail.com>
2025-03-19 08:51:28 +0300

    test

--- catsBot/handlers/user/test/callback_handlers.py
+++ catsBot/handlers/user/test/callback_handlers.py
@@ -29,10 +29,10 @@ async def question1(message: Message, state: FSMContext):
     data = await state.get_data()
     data[message.text] += 1
     await state.update_data(data)
-    await message.answer("""2. Какой характер кота вам ближе?
- А) Спокойный и ласковый.
- Б) Игривый и любопытный.
- В) Независимый и гордый.""", reply_markup=keyboards.test_reply_keyboard())
+    await message.answer("""<br>2. Какой характер кота вам ближе?</br>
+ <br>А)</br> Спокойный и ласковый.
+ <br>Б)</br> Игривый и любопытный.
+ <br>В)</br> Независимый и гордый.""", reply_markup=keyboards.test_reply_keyboard())
     await state.set_state(TestStates.question2)
 
 
@@ -41,10 +41,10 @@ async def question2(message: Message, state: FSMContext):
     data = await state.get_data()
     data[message.text] += 1
     await state.update_data(data)
-    await message.answer("""3. Сколько времени вы готовы уделять коту?
- А) Много времени, готов(а) играть и ухаживать.
- Б) Умеренно, но регулярно.
- В) Немного, но готов(а) обеспечить комфорт.""", reply_markup=keyboards.test_reply_keyboard())
+    await message.answer("""<br>3. Сколько времени вы готовы уделять коту?</br>
+ <br>А)</br> Много времени, готов(а) играть и ухаживать.
+ <br>Б)</br> Умеренно, но регулярно.
+ <br>В)</br> Немного, но готов(а) обеспечить комфорт.""", reply_markup=keyboards.test_reply_keyboard())
     await state.set_state(TestStates.question3)
 
 @router.message(F.text.in_(["А", "Б", "В"]), TestStates.question3)
@@ -52,10 +52,10 @@ async def question3(message: Message, state: FSMContext):
     data = await state.get_data()
     data[message.text] += 1
     await state.update_data(data)
-    await message.answer("""4. Какой окрас кота вам нравится?
- А) Серый, дымчатый, черный.
- Б) Рыжий, полосатый, пятнистый.
- В) Белый, пушистый.""", reply_markup=keyboards.test_reply_keyboard())
+    await message.answer("""<br>4. Какой окрас кота вам нравится?</br>
+ <br>А)</br> Серый, дымчатый, черный.
+ <br>Б)</br> Рыжий, полосатый, пятнистый.
+ <br>В)</br> Белый, пушистый.""", reply_markup=keyboards.test_reply_keyboard())
     await state.set_state(TestStates.question4)
 
 @router.message(F.text.in_(["А", "Б", "В"]), TestStates.question4)
@@ -63,10 +63,10 @@ async def question4(message: Message, state: FSMContext):
     data = await state.get_data()
     data[message.text] += 1
     await state.update_data(data)
-    await message.answer("""5. Какой темперамент кота вам подходит?
- А) Миролюбивый и послушный.
- Б) Активный и любопытный.
- В) Независимый и неприступный.""", reply_markup=keyboards.test_reply_keyboard())
+    await message.answer("""<br>5. Какой темперамент кота вам подходит?</br>
+ <br>А)</br> Миролюбивый и послушный.
+ <br>Б)</br> Активный и любопытный.
+ <br>В)</br> Независимый и неприступный.""", reply_markup=keyboards.test_reply_keyboard())
     await state.set_state(TestStates.question5)
 
 @router.message(F.text.in_(["А", "Б", "В"]), TestStates.question5)
@@ -74,10 +74,10 @@ async def question5(message: Message, state: FSMContext):
     data = await state.get_data()
     data[message.text] += 1
     await state.update_data(data)
-    await message.answer("""6. Есть ли у вас дети или другие животные?
- А) Да, нужен кот, который ладит с другими.
- Б) Нет, но хочу, чтобы кот был дружелюбным.
- В) Нет, предпочитаю кота, который не требует много внимания.""", reply_markup=keyboards.test_reply_keyboard())
+    await message.answer("""<br>6. Есть ли у вас дети или другие животные?</br>
+ <br>А)</br> Да, нужен кот, который ладит с другими.
+ <br>Б)</br> Нет, но хочу, чтобы кот был дружелюбным.
+ <br>В)</br> Нет, предпочитаю кота, который не требует много внимания.""", reply_markup=keyboards.test_reply_keyboard())
     await state.set_state(TestStates.question6)
 
 @router.message(F.text.in_(["А", "Б", "В"]), TestStates.question6)
@@ -85,10 +85,10 @@ async def question6(message: Message, state: FSMContext):
     data = await state.get_data()
     data[message.text] += 1
     await state.update_data(data)
-    await message.answer("""7. Готовы ли вы ухаживать за котом, который нуждается в лечении?
- А) Да, готов(а) помочь.
- Б) Нет, предпочитаю здорового кота.
- В) Возможно, если это не требует больших усилий.""", reply_markup=keyboards.test_reply_keyboard())
+    await message.answer("""<br>7. Готовы ли вы ухаживать за котом, который нуждается в лечении?</br>
+ <br>А)</br> Да, готов(а) помочь.
+ <br>Б)</br> Нет, предпочитаю здорового кота.
+ <br>В)</br> Возможно, если это не требует больших усилий.""", reply_markup=keyboards.test_reply_keyboard())
     await state.set_state(TestStates.answer)
 
 @router.message(TestStates.answer)
