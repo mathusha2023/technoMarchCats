56df5928c32c70d511b758053176bb1401ce6fb0
Alex Efremenkov <top.chel.05@mail.ru>
2025-03-19 09:32:39 +0300

    sss

--- catsBot/utils/main_info.py
+++ catsBot/utils/main_info.py
@@ -6,25 +6,10 @@ from data.animals_images import AnimalImage
 
 def get_animal_info(animal_name: str):
     session = create_session()
-    query = (
-        select(
-            Animal.name,
-            Animal.gender,
-            Animal.birthDate,
-            Animal.description,
-            AnimalImage,
-            func.group_concat(AnimalTag.name) # Агрегируем теги в строку
-        )
-        .join(AnimalImage, Animal.images, isouter=True)  # Присоединяем таблицу AnimalImage (LEFT JOIN)
-        .join(AnimalToAnimalTag, Animal.tags, isouter=True)  # Присоединяем таблицу AnimalToAnimalTag (LEFT JOIN)
-        .join(AnimalTag, AnimalToAnimalTag.c.animal_tag_id == AnimalTag.id, isouter=True)  # Присоединяем таблицу AnimalTag (LEFT JOIN)
-        .where(Animal.name == animal_name)
-        .group_by(Animal.id)  # Группируем по животному, чтобы агрегировать теги
-        .limit(1)  # Ограничиваем результат одним животным
-    )
-    data = session.execute(query).fetchone()._asdict()
-    if data is None:
-        return None
+    animal = session.query(Animal).where(Animal.name == animal_name).first()
+    data = {}  # сюда заносим данные животного в виде словаря
+    if animal is None:
+        return
     data["birthday"] = data["birthDate"]
     data["photos"] = [data["AnimalImage"]]
     return data
